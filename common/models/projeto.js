b7884e7d38239745e8e8c58eca5e9b26155ab42c
shsandro <shsandro@github.com>
2019-10-17 18:31:09 -0300

    bs datas implemented

--- common/models/projeto.js
+++ common/models/projeto.js
@@ -1,5 +1,38 @@
 'use strict';
 
 module.exports = function(Projeto) {
+  // Verifica se a data de termino não antecede a data de inicio
+  Projeto.observe('before-save', (ctx, next) => {
+    if (ctx.isNewInstance) {
+      const newProjeto = ctx.instance;
 
+      if (!newProjeto.dataTermino) {
+        return next();
+      } else {
+        const dataInicio = newProjeto.dataInicio.valueOf();
+        const dataTermino = newProjeto.dataTermino.valueOf();
+
+        if (dataInicio > dataTermino) {
+          const error = new Error();
+
+          error.message = 'Data de término antecede data de início.';
+          error.status = 400;
+
+          return next(error);
+        } else return next();
+      }
+    } else {
+      const dataInicio = ctx.data.dataInicio.valueOf();
+      const dataTermino = ctx.data.dataTermino.valueOf();
+
+      if (dataInicio > dataTermino) {
+        const error = new Error();
+
+        error.message = 'Data de término antecede data de início.';
+        error.status = 400;
+
+        return next(error);
+      } else return next();
+    }
+  });
 };
