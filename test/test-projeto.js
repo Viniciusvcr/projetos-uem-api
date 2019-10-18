7f9fdef3328e5ce6cd8d909cc76d7c5dc8cd9da2
shsandro <shsandro@github.com>
2019-10-18 15:29:14 -0300

    post test projetos for status code 200 passing

---
+++ test/test-projeto.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const request = require('request');
+const expect = require('chai').expect;
+const chai = require('chai');
+chai.use(require('chai-datetime'));
+
+const baseUrl = 'http://localhost:3001/api';
+
+const projetoPost = {
+  titulo: 'Pesquisa de software',
+  dataInicio: new Date(2019, 10, 1, 0, 0, 0, 0),
+  dataTermino: new Date(2020, 9, 30, 0, 0, 0, 0),
+  limiteParticipantes: 3,
+  resumo: 'Pesquisa sobre Engenharia de Software',
+  tipo: 'PIC',
+};
+
+describe('Testes Projetos', () => {
+  it('Deveria criar um projeto e retornar status code 200', done => {
+    request.post(
+      {
+        headers: {'content-type': 'application/json'},
+        url: `${baseUrl}/Projetos`,
+        body: JSON.stringify(projetoPost),
+      },
+      (error, response, body) => {
+        const obj = JSON.parse(response.body);
+
+        projetoPost['id'] = obj.id;
+        expect(response.statusCode).to.equal(200);
+        expect(obj.titulo).to.equal('Pesquisa de software');
+        expect(new Date(obj.dataInicio)).to.equalDate(
+          projetoPost['dataInicio']
+        );
+        expect(new Date(obj.dataTermino)).to.equalDate(
+          projetoPost['dataTermino']
+        );
+        expect(obj.atualParticipantes).to.equal(0);
+        expect(obj.limiteParticipantes).to.equal(3);
+        expect(obj.resumo).to.equal('Pesquisa sobre Engenharia de Software');
+        expect(obj.ativo).to.equal(true);
+        expect(obj.tipo).to.equal('PIC');
+        done();
+      }
+    );
+  });
+});
