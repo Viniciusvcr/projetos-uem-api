9a43d2e99649ae7080c8055867785d5203371475
shsandro <shsandro@github.com>
2019-10-18 13:52:05 -0300

    bs limites implemented

--- common/models/projeto.js
+++ common/models/projeto.js
@@ -2,7 +2,7 @@
 
 module.exports = function(Projeto) {
   // Verifica se a data de termino não antecede a data de inicio
-  Projeto.observe('before-save', (ctx, next) => {
+  Projeto.observe('before save', (ctx, next) => {
     if (ctx.isNewInstance) {
       const newProjeto = ctx.instance;
 
@@ -32,6 +32,45 @@ module.exports = function(Projeto) {
         error.status = 400;
 
         return next(error);
+      } else return next();
+    }
+  });
+
+  // Verifica se o numero atual de participantes respeita o limite maximo
+  Projeto.observe('before save', (ctx, next) => {
+    if (ctx.isNewInstance) {
+      const newProjeto = ctx.instance;
+
+      if (!newProjeto.limiteParticipantes) {
+        return next();
+      } else {
+        const limiteParticipantes = newProjeto.limiteParticipantes;
+        const atualParticipantes = newProjeto.atualParticipantes;
+
+        if (atualParticipantes > limiteParticipantes) {
+          const error = new Error();
+
+          error.message =
+            'Quantidade atual de participantes excede o limite permitido.';
+          error.status = 400;
+
+          return next(error);
+        } else return next();
+      }
+    } else {
+      const limiteParticipantes = ctx.data.limiteParticipantes;
+      const atualParticipantes = ctx.data.atualParticipantes;
+
+      if (limiteParticipantes) {
+        if (atualParticipantes > limiteParticipantes) {
+          const error = new Error();
+
+          error.message =
+            'Quantidade atual de participantes excede o limite permitido.';
+          error.status = 400;
+
+          return next(error);
+        } else return next();
       } else return next();
     }
   });
