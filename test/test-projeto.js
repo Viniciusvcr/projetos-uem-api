3729ec843d17b371aa9aedd233d5c257d7d563fb
shsandro <shsandro@github.com>
2019-10-18 22:06:54 -0300

    post test projetos for error 400 in datas passing

--- test/test-projeto.js
+++ test/test-projeto.js
@@ -16,6 +16,15 @@ const projetoPost = {
   tipo: 'PIC',
 };
 
+const projetoDateError = {
+  titulo: 'Pesquisa de software',
+  dataInicio: new Date(2020, 10, 1, 0, 0, 0, 0),
+  dataTermino: new Date(2019, 9, 30, 0, 0, 0, 0),
+  limiteParticipantes: 3,
+  resumo: 'Pesquisa sobre Engenharia de Software',
+  tipo: 'PIC',
+};
+
 describe('Testes Projetos', () => {
   it('Deveria criar um projeto e retornar status code 200', done => {
     request.post(
@@ -41,6 +50,26 @@ describe('Testes Projetos', () => {
         expect(obj.resumo).to.equal('Pesquisa sobre Engenharia de Software');
         expect(obj.ativo).to.equal(true);
         expect(obj.tipo).to.equal('PIC');
+        done();
+      }
+    );
+  });
+  // eslint-disable-next-line max-len
+  it('Deveria criar um projeto e retornar status code 400 - Falha ao colocar uma data de início maior que a data de término', done => {
+    request.post(
+      {
+        headers: {'content-type': 'application/json'},
+        url: `${baseUrl}/Projetos`,
+        body: JSON.stringify(projetoDateError),
+      },
+      (error, response, body) => {
+        const obj = JSON.parse(response.body);
+
+        projetoDateError['id'] = obj.id;
+        expect(response.statusCode).to.equal(400);
+        expect(obj.error.message).to.equal(
+          'Data de término antecede data de início.'
+        );
         done();
       }
     );
