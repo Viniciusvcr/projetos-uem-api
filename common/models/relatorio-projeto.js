7b9d8ca2de38ed73860f855c13f55f67c085ac46
shsandro <shsandro@github.com>
2019-10-21 16:42:21 -0300

    remote method revelancia for projeto implemented

--- common/models/relatorio-projeto.js
+++ common/models/relatorio-projeto.js
@@ -13,7 +13,7 @@ module.exports = function(Relatorioprojeto) {
   Relatorioprojeto.observe('before save', (ctx, next) => {
     if (ctx.isNewInstance) {
       Relatorioprojeto.findOne(
-        {where: {projetoId: ctx.projetoId}},
+        {where: {projetoId: ctx.instance.projetoId}},
         (err, obj) => {
           if (err) return next(err);
 
@@ -43,10 +43,8 @@ module.exports = function(Relatorioprojeto) {
         const months = monthDiff(dataAtual, dataRelatorio);
 
         if (months == 0) {
-          console.log('Zero');
           return cb(null, relatorio.numeroAcessos);
         } else {
-          console.log('N zero');
           const media = relatorio.numeroAcessos / months;
 
           return cb(null, media);
@@ -65,5 +63,40 @@ module.exports = function(Relatorioprojeto) {
     returns: {arg: 'response', type: 'number', root: true},
     http: {path: '/mediaAcessos', verb: 'get', status: 200},
     description: 'Retorna média de acessos por mês de um projeto.',
+  });
+
+  Relatorioprojeto.relevancia = async function(projetoId, cb) {
+    try {
+      const relatorio = await Relatorioprojeto.findOne({where: {projetoId}});
+
+      const relatorios = await Relatorioprojeto.find();
+
+      let totalAcessos = 0;
+
+      for (const obj of relatorios) {
+        totalAcessos += obj.numeroAcessos;
+      }
+
+      if (totalAcessos == 0) {
+        return cb(null, 0);
+      } else {
+        const relevancia = relatorio.numeroAcessos / totalAcessos;
+        return cb(null, relevancia);
+      }
+    } catch (err) {
+      return cb(err);
+    }
+  };
+
+  Relatorioprojeto.remoteMethod('relevancia', {
+    accepts: {
+      arg: 'projetoId',
+      type: 'number',
+      required: true,
+      description: 'ID do projeto',
+    },
+    returns: {arg: 'response', type: 'number', root: true},
+    http: {path: '/relevancia', verb: 'get', status: 200},
+    description: 'Retorna a relevância de um projeto nos acessos totais.',
   });
 };
