114aee4862127c8ac1edc3e90c25d3ca6887b1bb
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-10-26 01:47:37 -0300

    script de atualização do banco de dados refatorado com async/await

--- server/boot/01-init.js
+++ server/boot/01-init.js
@@ -1,129 +1,134 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = async function(app) {
   const config = require('../data/config.json');
+  const boldWhite = '\x1b[1m';
+  const boldRed = '\x1b[1;31m';
+  const boldGreen = '\x1b[1;32m';
+  const reset = '\x1b[0m';
 
   const informations = function() {
-    const boldWhite = '\x1b[1m';
-    const boldRed = '\x1b[1;31m';
-    const reset = '\x1b[0m';
     const db = app.dataSources.db;
-    // const email =
-    //   app.dataSources.email.connector.dataSource.settings.transports[0];
 
-    console.log(`${boldWhite}Ambiente: ${boldRed}'${process.env.NODE_ENV}'${reset}`);
+    console.log(`${boldWhite}Informações da API:${reset}`);
+
+    console.log(`  ${boldWhite}Ambiente: ${boldRed}'${process.env.NODE_ENV}'${reset}`);
     console.log(
       // eslint-disable-next-line max-len
-      `${boldWhite}Banco de dados: ${boldRed}'${db.connector.name}:${db.connector.settings.database}'${reset}`
+      `  ${boldWhite}Banco de dados: ${boldRed}'${db.connector.name}:${db.connector.settings.database}'${reset}`
     );
-    // console.log(
-    //   `${boldWhite}Email: ${boldRed}'${email.host.split('.')[1]}:${
-    //     email.auth.user
-    //   }'${reset}`
-    // );
   };
 
-  const upsertAdmin = function() {
+  const upsertAdmin = async () => {
     const Usuario = app.models.Usuario;
     const RoleMapping = app.models.RoleMapping;
     const Role = app.models.Role;
     const Admin = config.admin;
 
-    Usuario.upsertWithWhere({username: Admin.username}, Admin, (err, user) => {
-      if (err) {
-        throw err;
-      }
+    try {
+      const user = await Usuario.upsertWithWhere({username: Admin.username}, Admin);
+      const role = await Role.findOne({where: {name: 'admin'}});
 
-      Role.findOne({where: {name: 'admin'}}, (err, role) => {
-        if (err) {
-          throw err;
-        }
+      if (!role) {
+        throw new Error('Role admin não encontrada');
+      } else {
+        const [instance, created] = await RoleMapping.findOrCreate(
+          {where: {principalId: user.id, roleId: role.id}},
+          {
+            principalType: RoleMapping.USER,
+            principalId: user.id,
+            roleId: role.id
+          }
+        );
 
-        if (!role) {
-          throw new Error('Role admin não encontrada');
-        } else {
-          RoleMapping.findOrCreate(
-            {where: {principalId: user.id, roleId: role.id}},
-            {
-              principalType: RoleMapping.USER,
-              principalId: user.id,
-              roleId: role.id
-            },
-            (err, instance, created) => {
-              if (err) {
-                throw err;
-              }
-
-              if (created) {
-                console.log(`    ${user.username} se tornou um ${role.name}!`);
-              } else {
-                console.log(`    Admin ${user.username} atualizado!`);
-              }
-            }
+        if (created) {
+          console.log(
+            // eslint-disable-next-line max-len
+            `    '${boldGreen}${user.username}${reset}' se tornou um '${boldGreen}${role.name}${reset}'!`
           );
+        } else {
+          console.log(`    Admin '${boldGreen}${user.username}${reset}' atualizado!`);
         }
-      });
-    });
+      }
+    } catch (err) {
+      throw err;
+    }
   };
 
-  const initRoles = function() {
+  const initRoles = async () => {
     const Role = app.models.Role;
     const roles = config.roles;
 
-    roles.forEach(actual => {
-      Role.findOrCreate(
-        {where: {name: actual}},
-        {name: actual},
-        (err, roleInstance, roleCreated) => {
-          if (err) throw err;
+    for (const role of roles) {
+      try {
+        const [roleInstance, roleCreated] = await Role.findOrCreate(
+          {where: {name: role}},
+          {name: role}
+        );
 
-          if (roleCreated) {
-            console.log(`    Role "${roleInstance.name}" criada!`);
-          } else {
-            console.log(`    "${roleInstance.name}" já existe no Banco de Dados.`);
-          }
+        if (roleCreated) {
+          console.log(`    Role '${boldGreen}${roleInstance.name}${reset}' criada!`);
+        } else {
+          console.log(
+            `    ${boldGreen}Role '${roleInstance.name}' já existe no Banco de Dados.${reset}`
+          );
         }
-      );
-    });
+      } catch (err) {
+        throw err;
+      }
+    }
   };
 
   const modelActions = function(model) {
     if (model === 'Role') {
-      initRoles();
+      return initRoles();
     } else if (model === 'Usuario') {
-      upsertAdmin();
+      return upsertAdmin();
     }
   };
 
-  const initDatabase = function() {
-    const mysql = app.dataSources.db;
-    const models = mysql.modelBuilder.models;
-
-    Object.keys(models).forEach(model => {
-      const modelDs = models[model].getDataSource();
-
-      if (modelDs) {
-        if (model != 'Email') {
-          mysql.isActual(model, (err, actual) => {
-            if (err) throw err;
-
-            if (actual) {
-              console.log(`  Modelo "${model}" está atualizado no Banco de Dados`);
-              modelActions(model);
-            } else {
-              mysql.autoupdate(model, (err, result) => {
-                if (err) throw err;
-
-                modelActions(model);
-                console.log(`  '${model}' atualizado com sucesso!`);
-              });
-            }
-          });
-        }
-      }
+  const isActual = (dataSource, model) => {
+    return new Promise((resolve, reject) => {
+      dataSource.isActual(model, (err, actual) => {
+        if (err) reject(err);
+        resolve(actual);
+      });
     });
   };
 
+  const initDatabase = async () => {
+    const mysql = app.dataSources.db;
+    const models = mysql.modelBuilder.models;
+
+    console.log();
+    console.log(`${boldWhite}Atualizando automaticamente os modelos no Banco de Dados...${reset}`);
+
+    for (const model of Object.keys(models)) {
+      const modelDS = models[model].getDataSource();
+
+      if (modelDS) {
+        try {
+          const actual = await isActual(mysql, model);
+
+          if (actual) {
+            console.log(
+              `  ${boldWhite}Modelo '${model}' está atualizado no Banco de Dados${reset}`
+            );
+          } else {
+            await mysql.autoupdate(model);
+            console.log(`  ${boldGreen}${model} atualizado com sucesso!${reset}`);
+          }
+
+          await modelActions(model);
+        } catch (err) {
+          throw err;
+        }
+      }
+    }
+  };
+
+  await initDatabase();
+  console.log();
   informations();
-  initDatabase();
+  console.log();
 };
