438aafcdbab026e40a2664768e03bc414549e078
Arthur <arthurbatista04@outlook.com>
2019-10-27 20:28:34 -0300

    duplicata de inscrito no processo seleitvo

--- common/models/inscrito.js
+++ common/models/inscrito.js
@@ -5,26 +5,26 @@ module.exports = function(Inscrito) {
   Inscrito.observe('before save', (ctx, next) => {
     if (ctx.isNewInstance) {
       const newInscrito = ctx.instance;
-      const Usuario = Inscrito.app.models.Usuario;
+      const Discente = Inscrito.app.models.Discente;
 
       Inscrito.findOne(
-        {where: {projetoId: newInscrito.projetoId, usuarioId: newInscrito.usuarioId}},
+        {where: {projetoId: newInscrito.projetoId, discenteId: newInscrito.discenteId}},
         (err, inscrito) => {
           if (err) return next(err);
           if (inscrito) {
-            Usuario.findById(inscrito.usuarioId, (err, usuario) => {
+            Discente.findById(inscrito.discenteId, (err, discente) => {
               if (err) return next(err);
-              if (!usuario) {
+              if (!discente) {
                 const error = new Error();
 
-                error.message = 'Usuário não encontrado';
+                error.message = 'Discente não encontrado';
                 error.status = 404;
 
                 return next(error);
               } else {
                 const error = new Error();
 
-                error.message = `${usuario.nome} já inscrito no evento`;
+                error.message = 'Você já está inscrito no processo seletivo';
                 error.status = 409;
 
                 return next(error);
