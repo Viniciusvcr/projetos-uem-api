55262415589d0b8a0ffbc1545868e7aeb291dfc5
Vinícius Regatieri <viniciusvcr@hotmail.com>
2019-11-15 02:05:17 -0300

    testes automatizados de envio de email

--- test/test-usuario.js
+++ test/test-usuario.js
@@ -1,85 +1,91 @@
-'use strict';
+"use strict";
 
-const request = require('request');
-const expect = require('chai').expect;
+// Desenvolvedor: Vinícius Regatieri
 
-const baseUrl = 'http://localhost:3001/api';
+const request = require("request");
+const rp = require("request-promise-native");
+const expect = require("chai").expect;
+
+const baseUrl = "http://localhost:3001/api";
 
 const postTest = {
-  nome: 'Vinícius',
-  realm: 'Normal',
-  username: 'teste',
-  email: 'teste@teste.com',
-  password: '123',
+  nome: "Vinícius",
+  realm: "Normal",
+  username: "teste",
+  email: "teste@teste.com",
+  password: "123",
   emailVerified: false
 };
 
 const emailInvalidoTest = {
-  nome: 'Vinícius',
-  realm: 'Normal',
-  username: 'teste',
-  email: 'naoeumemail',
-  password: '123'
+  nome: "Vinícius",
+  realm: "Normal",
+  username: "teste",
+  email: "naoeumemail",
+  password: "123"
 };
 
 const updateTest = {
-  nome: 'Vinícius Regatieri',
-  realm: 'Normal',
-  username: 'teste',
-  email: 'teste@teste.com',
-  password: '123',
+  nome: "Vinícius Regatieri",
+  realm: "Normal",
+  username: "teste",
+  email: "teste@teste.com",
+  password: "123",
   emailVerified: false
 };
 
 const roleTest = {
-  nome: 'Vinícius Regatieri',
-  realm: 'RoleInexistente',
-  username: 'teste',
-  email: 'teste@teste.com',
-  password: '123',
+  nome: "Vinícius Regatieri",
+  realm: "RoleInexistente",
+  username: "teste",
+  email: "teste@teste.com",
+  password: "123",
   emailVerified: false
 };
 
-describe('Modelo Usuário', () => {
-  describe('Testes CRUD', () => {
-    it('Deveria criar um novo usuário e retornar status 200', done => {
+describe("Modelo Usuário", () => {
+  describe("Testes CRUD", () => {
+    it("Deveria criar um novo usuário e retornar status 200", done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios`,
           body: JSON.stringify(postTest)
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
 
-          postTest['id'] = obj.id;
+          postTest["id"] = obj.id;
           expect(response.statusCode).to.equal(200);
-          expect(obj.nome).to.equal('Vinícius');
-          expect(obj.realm).to.equal('Normal');
-          expect(obj.username).to.equal('teste');
-          expect(obj.email).to.equal('teste@teste.com');
+          expect(obj.nome).to.equal("Vinícius");
+          expect(obj.realm).to.equal("Normal");
+          expect(obj.username).to.equal("teste");
+          expect(obj.email).to.equal("teste@teste.com");
           expect(obj.emailVerified).to.equal(false);
           done();
         }
       );
     });
 
-    it('Deveria retornar status 200 e atualizar atributos ao atualizar um Usuário', done => {
+    it("Deveria retornar status 200 e atualizar atributos ao atualizar um Usuário", done => {
       request.post(
         {
           headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
           },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: postTest.username, password: postTest.password})
+          body: JSON.stringify({
+            username: postTest.username,
+            password: postTest.password
+          })
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
           request.patch(
             {
               headers: {
-                'content-type': 'application/json',
-                Accept: 'application/json'
+                "content-type": "application/json",
+                Accept: "application/json"
               },
               url: `${baseUrl}/Usuarios/${obj.userId}?access_token=${obj.id}`,
               body: JSON.stringify(updateTest)
@@ -87,10 +93,10 @@ describe('Modelo Usuário', () => {
             (error, responsePatch, body) => {
               const obj = JSON.parse(responsePatch.body);
               expect(responsePatch.statusCode).to.equal(200);
-              expect(obj.nome).to.equal('Vinícius Regatieri');
-              expect(obj.realm).to.equal('Normal');
-              expect(obj.username).to.equal('teste');
-              expect(obj.email).to.equal('teste@teste.com');
+              expect(obj.nome).to.equal("Vinícius Regatieri");
+              expect(obj.realm).to.equal("Normal");
+              expect(obj.username).to.equal("teste");
+              expect(obj.email).to.equal("teste@teste.com");
               expect(obj.emailVerified).to.equal(false);
               done();
             }
@@ -99,32 +105,35 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Deveria retornar status 200 o Usuário cadastrado nos testes', done => {
+    it("Deveria retornar status 200 o Usuário cadastrado nos testes", done => {
       request.post(
         {
           headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
           },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: postTest.username, password: postTest.password})
+          body: JSON.stringify({
+            username: postTest.username,
+            password: postTest.password
+          })
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
           request.get(
             {
               headers: {
-                'content-type': 'application/json',
-                Accept: 'application/json'
+                "content-type": "application/json",
+                Accept: "application/json"
               },
               url: `${baseUrl}/Usuarios/${obj.userId}?access_token=${obj.id}`
             },
             (error, responsePatch, body) => {
               const obj = JSON.parse(responsePatch.body);
               expect(responsePatch.statusCode).to.equal(200);
-              expect(obj.nome).to.equal('Vinícius Regatieri');
-              expect(obj.realm).to.equal('Normal');
-              expect(obj.username).to.equal('teste');
-              expect(obj.email).to.equal('teste@teste.com');
+              expect(obj.nome).to.equal("Vinícius Regatieri");
+              expect(obj.realm).to.equal("Normal");
+              expect(obj.username).to.equal("teste");
+              expect(obj.email).to.equal("teste@teste.com");
               expect(obj.emailVerified).to.equal(false);
               done();
             }
@@ -133,22 +142,25 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Deveria retornar status 200 e count 1 ao remover um Usuário', done => {
+    it("Deveria retornar status 200 e count 1 ao remover um Usuário", done => {
       request.post(
         {
           headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
           },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: postTest.username, password: postTest.password})
+          body: JSON.stringify({
+            username: postTest.username,
+            password: postTest.password
+          })
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
           request.delete(
             {
               headers: {
-                'content-type': 'application/json',
-                Accept: 'application/json'
+                "content-type": "application/json",
+                Accept: "application/json"
               },
               url: `${baseUrl}/Usuarios/${obj.userId}?access_token=${obj.id}`,
               body: JSON.stringify(updateTest)
@@ -165,13 +177,13 @@ describe('Modelo Usuário', () => {
     });
   });
 
-  describe('Testes de duplicação e integridade de dados', () => {
-    it('Não é permitido deletar um Usuário sem se identificar', done => {
+  describe("Testes de duplicação e integridade de dados", () => {
+    it("Não é permitido deletar um Usuário sem se identificar", done => {
       request.delete(
         {
           headers: {
-            'content-type': 'application/json',
-            Accept: 'application/json'
+            "content-type": "application/json",
+            Accept: "application/json"
           },
           url: `${baseUrl}/Usuarios/qualquerid`
         },
@@ -183,12 +195,12 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Não é permitido atualizar atributos de um Usuário sem se identificar', done => {
+    it("Não é permitido atualizar atributos de um Usuário sem se identificar", done => {
       request.patch(
         {
           headers: {
-            'content-type': 'application/json',
-            Accept: 'application/json'
+            "content-type": "application/json",
+            Accept: "application/json"
           },
           url: `${baseUrl}/Usuarios/qualquerid`,
           body: JSON.stringify(updateTest)
@@ -201,10 +213,10 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Deveria retornar 404 caso a função passada no cadastro não exista', done => {
+    it("Deveria retornar 404 caso a função passada no cadastro não exista", done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios`,
           body: JSON.stringify(roleTest)
         },
@@ -212,7 +224,7 @@ describe('Modelo Usuário', () => {
           const obj = JSON.parse(response.body);
 
           expect(response.statusCode).to.equal(404);
-          expect(obj.error.message).to.equal('Realm não encontrada');
+          expect(obj.error.message).to.equal("Realm não encontrada");
           done();
         }
       );
@@ -221,7 +233,7 @@ describe('Modelo Usuário', () => {
     it("Deveria retornar status 400 e a mensagem 'Email inválido' ao tentar cadastrar um email inválido", done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios`,
           body: JSON.stringify(emailInvalidoTest)
         },
@@ -229,18 +241,18 @@ describe('Modelo Usuário', () => {
           const obj = JSON.parse(response.body);
 
           expect(response.statusCode).to.equal(400);
-          expect(obj.error.message).to.equal('Email inválido');
+          expect(obj.error.message).to.equal("Email inválido");
           done();
         }
       );
     });
   });
 
-  describe('Testes gerais', () => {
-    it('Deveria retornar status 200, chave de acesso à plataforma e id do usuário ao fazer login', done => {
+  describe("Testes gerais", () => {
+    it("Deveria retornar status 200, chave de acesso à plataforma e id do usuário ao fazer login", done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios`,
           body: JSON.stringify(postTest)
         },
@@ -248,10 +260,13 @@ describe('Modelo Usuário', () => {
           request.post(
             {
               headers: {
-                'content-type': 'application/json'
+                "content-type": "application/json"
               },
               url: `${baseUrl}/Usuarios/login`,
-              body: JSON.stringify({username: postTest.username, password: postTest.password})
+              body: JSON.stringify({
+                username: postTest.username,
+                password: postTest.password
+              })
             },
             (error, response, body) => {
               const obj = JSON.parse(response.body);
@@ -269,26 +284,29 @@ describe('Modelo Usuário', () => {
     it('Deveria retornar status 401 e código "LOGIN_FAILED" ao tentar fazer login com credenciais inválidas ou sem se cadastrar', done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: 'teste', password: '111'})
+          body: JSON.stringify({ username: "teste", password: "111" })
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
 
           expect(response.statusCode).to.equal(401);
-          expect(obj.error.code).to.equal('LOGIN_FAILED');
+          expect(obj.error.code).to.equal("LOGIN_FAILED");
           done();
         }
       );
     });
 
-    it('Deveria retornar 204 ao deslogar da plataforma usando a chave de acesso', done => {
+    it("Deveria retornar 204 ao deslogar da plataforma usando a chave de acesso", done => {
       request.post(
         {
-          headers: {'content-type': 'application/json'},
+          headers: { "content-type": "application/json" },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: postTest.username, password: postTest.password})
+          body: JSON.stringify({
+            username: postTest.username,
+            password: postTest.password
+          })
         },
         (error, response, body) => {
           const obj = JSON.parse(response.body);
@@ -297,8 +315,8 @@ describe('Modelo Usuário', () => {
           request.post(
             {
               headers: {
-                'content-type': 'application/json',
-                Accept: 'application/json',
+                "content-type": "application/json",
+                Accept: "application/json",
                 Authorization: token
               },
               url: `${baseUrl}/Usuarios/logout`
@@ -312,12 +330,12 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Deveria retornar 401 ao deslogar da plataforma sem a chave de acesso', done => {
+    it("Deveria retornar 401 ao deslogar da plataforma sem a chave de acesso", done => {
       request.post(
         {
           headers: {
-            'content-type': 'application/json',
-            Accept: 'application/json'
+            "content-type": "application/json",
+            Accept: "application/json"
           },
           url: `${baseUrl}/Usuarios/logout`
         },
@@ -328,14 +346,17 @@ describe('Modelo Usuário', () => {
       );
     });
 
-    it('Deveria retornar status 204 e atualizar a senha de um Usuário', done => {
+    it("Deveria retornar status 204 e atualizar a senha de um Usuário", done => {
       request.post(
         {
           headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
           },
           url: `${baseUrl}/Usuarios/login`,
-          body: JSON.stringify({username: postTest.username, password: postTest.password})
+          body: JSON.stringify({
+            username: postTest.username,
+            password: postTest.password
+          })
         },
         (error, responseLogin1, bodyLogin1) => {
           const obj = JSON.parse(responseLogin1.body);
@@ -343,12 +364,15 @@ describe('Modelo Usuário', () => {
           request.post(
             {
               headers: {
-                'content-type': 'application/json',
-                Accept: 'application/json',
+                "content-type": "application/json",
+                Accept: "application/json",
                 Authorization: token
               },
               url: `${baseUrl}/Usuarios/change-password`,
-              body: JSON.stringify({oldPassword: postTest.password, newPassword: '1234'})
+              body: JSON.stringify({
+                oldPassword: postTest.password,
+                newPassword: "1234"
+              })
             },
             (error, response, body) => {
               expect(response.statusCode).to.equal(204);
@@ -356,10 +380,13 @@ describe('Modelo Usuário', () => {
               request.post(
                 {
                   headers: {
-                    'content-type': 'application/json'
+                    "content-type": "application/json"
                   },
                   url: `${baseUrl}/Usuarios/login`,
-                  body: JSON.stringify({username: postTest.username, password: '1234'})
+                  body: JSON.stringify({
+                    username: postTest.username,
+                    password: "1234"
+                  })
                 },
                 (error, responseLogin2, bodyLogin2) => {
                   const obj = JSON.parse(responseLogin2.body);
@@ -374,6 +401,58 @@ describe('Modelo Usuário', () => {
           );
         }
       );
+    });
+
+    it("Deveria retornar 204 ao tentar enviar um email logado no sistema", async () => {
+      try {
+        const loginDiscente = await rp.post({
+          headers: {
+            "content-type": "application/json",
+            Accept: "application/json"
+          },
+          url: `${baseUrl}/Usuarios/login`,
+          body: JSON.stringify({
+            username: "admin",
+            password: "admin"
+          })
+        });
+
+        const token = JSON.parse(loginDiscente).id;
+        await rp.post({
+          headers: {
+            "content-type": "application/json",
+            Accept: "application/json",
+            Authorization: token
+          },
+          url: `${baseUrl}/Usuarios/send-email`,
+          body: JSON.stringify({
+            to: "teste",
+            subject: "teste",
+            text: "teste"
+          })
+        });
+      } catch (err) {
+        throw err;
+      }
+    });
+
+    it("Deveria retornar 401 ao tentar enviar um email sem se logar no sistema", async () => {
+      try {
+        await rp.post({
+          headers: {
+            "content-type": "application/json",
+            Accept: "application/json"
+          },
+          url: `${baseUrl}/Usuarios/send-email`,
+          body: JSON.stringify({
+            to: "teste",
+            subject: "teste",
+            text: "teste"
+          })
+        });
+      } catch (err) {
+        expect(err.statusCode).to.equal(401);
+      }
     });
   });
 });
