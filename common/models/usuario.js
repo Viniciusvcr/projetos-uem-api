a2463cd93d8174c7319e0c493d8334ec63d04326
deelefrati <douglasdelefrati@gmail.com>
2019-11-19 11:47:19 -0300

    criado afterRemote do numero de usuarios cirados

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const verifyEmail = require('isemail');
+const verifyEmail = require("isemail");
 
 const error = function(message, status) {
   const error = new Error();
@@ -13,30 +13,38 @@ const error = function(message, status) {
 
 module.exports = function(Usuario) {
   // Verifica se o email fornecido tem a forma de um email
-  Usuario.observe('before save', (ctx, next) => {
-    const newUsuario = ctx.isNewInstance ? ctx.instance : ctx.data ? ctx.data : ctx.instance;
+  Usuario.observe("before save", (ctx, next) => {
+    const newUsuario = ctx.isNewInstance
+      ? ctx.instance
+      : ctx.data
+      ? ctx.data
+      : ctx.instance;
 
     if (newUsuario.email) {
       if (!verifyEmail.validate(newUsuario.email)) {
-        return next(error('Email inválido', 400));
+        return next(error("Email inválido", 400));
       } else return next();
     } else return next();
   });
 
   // Verifica a integridade do atributo Realm
-  Usuario.observe('before save', (ctx, next) => {
-    const newUsuario = ctx.isNewInstance ? ctx.instance : ctx.data ? ctx.data : ctx.instance;
-    const roles = require('../../server/data/config.json').roles;
+  Usuario.observe("before save", (ctx, next) => {
+    const newUsuario = ctx.isNewInstance
+      ? ctx.instance
+      : ctx.data
+      ? ctx.data
+      : ctx.instance;
+    const roles = require("../../server/data/config.json").roles;
 
     if (newUsuario.realm) {
       if (roles.includes(newUsuario.realm)) {
         return next();
-      } else return next(error('Realm não encontrada', 404));
+      } else return next(error("Realm não encontrada", 404));
     } else return next();
   });
 
   // Aplica a função passada no cadastro
-  Usuario.observe('after save', (ctx, next) => {
+  Usuario.observe("after save", (ctx, next) => {
     if (ctx.isNewInstance) {
       const Role = Usuario.app.models.Role;
       const RoleMapping = Usuario.app.models.RoleMapping;
@@ -45,11 +53,11 @@ module.exports = function(Usuario) {
       const realmName = user.realm;
       const id = user.id;
 
-      Role.findOne({where: {name: realmName}}, (err, role) => {
+      Role.findOne({ where: { name: realmName } }, (err, role) => {
         if (err) return next(err);
 
         RoleMapping.findOrCreate(
-          {where: {principalType: RoleMapping.USER, principalId: id}},
+          { where: { principalType: RoleMapping.USER, principalId: id } },
           {
             principalType: RoleMapping.USER,
             principalId: id,
@@ -72,5 +80,20 @@ module.exports = function(Usuario) {
         );
       });
     } else return next();
+  });
+
+  Usuario.afterRemote("create", (ctx, userInstance, next) => {
+    const relariosAdmin = Usuario.app.models.relatoriosAdmin;
+    relariosAdmin.updateAttribute(
+      "qntdUsuariosCriados",
+      relariosAdmin.qntdUsuariosCriados + 1,
+      (err, instance) => {
+        if (err) {
+          return next(err);
+        } else {
+          return next();
+        }
+      }
+    );
   });
 };
